7e9d73eaec3c969ceccd56d811d2e76376ef82cb
ParanoidSyntaxError <71923508+ParanoidSyntaxError@users.noreply.github.com>
2024-05-04 03:42:30 +1000

    Fixed thirdweb

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
+import {
+  ConnectButton,
+} from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import {
+  createWallet,
+  walletConnect,
+  inAppWallet,
+  preAuthenticate
+} from "thirdweb/wallets";
+
+const client = createThirdwebClient({
+  clientId: "a02abdc106da3c1467ba518489b891dc",
+});
 
 const wallets = [
   walletConnect(),
@@ -12,53 +26,71 @@ const wallets = [
 ];
 
 function Home() {
-    const [amount, setAmount] = useState('')
+  const [amount, setAmount] = useState('')
 
-    const buttonPress = (e) => {
-        // console.log(e.target.textContent)
-        if (e.target.textContent !== 'Back'){
-            setAmount(amount + e.target.textContent)
-        }else{
-            setAmount(amount.slice(0, -1))
-        }
+  const buttonPress = (e) => {
+    // console.log(e.target.textContent)
+    if (e.target.textContent !== 'Back') {
+      setAmount(amount + e.target.textContent)
+    } else {
+      setAmount(amount.slice(0, -1))
     }
+  }
   return (
     <div className='bg-[#C8F73A] relative h-screen overflow-hidden'>
-        <div className='h-[10%]  p-5 mt-5 flex justify-center items-center'>
-            <h1 className='text-[54px] text-[#000]  font-bold'>ZEKE</h1>
-        </div>
-        
-        <div className='w-full h-[75%] text-md items-center flex flex-col justify-center'>
-            {/* <img src='https://cdn-icons-png.flaticon.com/512/1796/1796801.png' className='w-[50%] aspect-square rounded-2xl'/> */}
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣼⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣾⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⢿⣿⡿⠋⠉⠁⠀⠙⢆⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢀⡽⠁⠀⠀⠀⠀⠀⠈⢳⡀⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠙⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡿⣿⣿⣏⣿⣿⣾⣿⣿⠘⣿⣿⣿⣿⣏</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⢹⡇⣸⣿⣿⡿⡟⠀⢹⣿⣤⣿⣿⡟⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⡄⠀⢸⣿⣿⣿⣿⠶⡿⠿⠟⣹⠴⠟⢹⠃⣿⡉</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣄⣀⣠⣭⣿⠿⣿⣿⣿⠇⠀⣸⠙⠻⠿⢏⣱⠇⠀⠉⠀⠀⠀⡌⠀⢋⡇</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠦⠀⠈⠋⠀⠚⢉⠟⠁⠀⠀⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣶⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡾⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⢿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡜⢦⠀⠀⠀⠀⠀⢀⠤⠔⠚⠛⠉⠉⠉⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣷⣄⠀⠀⠀⠁⠀⠀⠤⠤⠴⠚⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡏⢱⡆⢹⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⢀⣴⣿⣿⠋⣤⡟⠁⢸⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣴⣿⣿⣿⣧⡾⠋⠀⠀⢸⣍⠻</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⢸⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠻⢿⣿⠟⣯⠉⠙⢧⠀⠀⠀⠀⠀⣿⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⣠⠴⣿⠋⠀⠹⣇⠀⠀⡇⠀⠀⠀⠀⣻⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢠⠞⠁⢠⡇⠈⡇⠀⢿⠀⠀⠸⠀⠀⠀⠀⠀⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠏⠀⠀⢸⡇⠀⡇⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⣿</h1>
-            <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡇⢺⠀⠀⠀⢸⡇⠀⣹⠀⠈⡇⠀⠀⢸⡄⠀⠀⠀⣿</h1>
-            {/* <h1>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⣾⣵⣿⣿⣯⢿⣷⡀⠈⠿⣮⡳⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h1>
+      <div className='h-[10%]  p-5 mt-5 flex justify-center items-center'>
+        <h1 className='text-[54px] text-[#000]  font-bold'>ZEKE</h1>
+      </div>
+      <div className='text-center'>
+        <ConnectButton
+          client={client}
+          chain={baseSepolia}
+          theme={"dark"}
+          connectModal={{
+            size: "compact",
+            title: "Zeke",
+            titleIcon: "",
+            showThirdwebBranding: false,
+          }}
+          wallets={wallets}
+          accountAbstraction={{
+            chain: baseSepolia,
+            factoryAddress: "0xb26BE4505119107f9Bb6D4948360E4B7Dea50e29",
+            gasless: true,
+          }}
+        />
+      </div>
+      <div className='w-full h-[75%] text-md items-center flex flex-col justify-center'>
+        {/* <img src='https://cdn-icons-png.flaticon.com/512/1796/1796801.png' className='w-[50%] aspect-square rounded-2xl'/> */}
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣼⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣾⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⢿⣿⡿⠋⠉⠁⠀⠙⢆⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢀⡽⠁⠀⠀⠀⠀⠀⠈⢳⡀⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠙⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡿⣿⣿⣏⣿⣿⣾⣿⣿⠘⣿⣿⣿⣿⣏</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⢹⡇⣸⣿⣿⡿⡟⠀⢹⣿⣤⣿⣿⡟⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⡄⠀⢸⣿⣿⣿⣿⠶⡿⠿⠟⣹⠴⠟⢹⠃⣿⡉</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣄⣀⣠⣭⣿⠿⣿⣿⣿⠇⠀⣸⠙⠻⠿⢏⣱⠇⠀⠉⠀⠀⠀⡌⠀⢋⡇</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠦⠀⠈⠋⠀⠚⢉⠟⠁⠀⠀⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣶⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡾⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⢿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡜⢦⠀⠀⠀⠀⠀⢀⠤⠔⠚⠛⠉⠉⠉⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣷⣄⠀⠀⠀⠁⠀⠀⠤⠤⠴⠚⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡏⢱⡆⢹⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⢀⣴⣿⣿⠋⣤⡟⠁⢸⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣴⣿⣿⣿⣧⡾⠋⠀⠀⢸⣍⠻</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⢸⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠻⢿⣿⠟⣯⠉⠙⢧⠀⠀⠀⠀⠀⣿⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⣠⠴⣿⠋⠀⠹⣇⠀⠀⡇⠀⠀⠀⠀⣻⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢠⠞⠁⢠⡇⠈⡇⠀⢿⠀⠀⠸⠀⠀⠀⠀⠀⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠏⠀⠀⢸⡇⠀⡇⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⣿</h1>
+        <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡇⢺⠀⠀⠀⢸⡇⠀⣹⠀⠈⡇⠀⠀⢸⡄⠀⠀⠀⣿</h1>
+        {/* <h1>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⣾⣵⣿⣿⣯⢿⣷⡀⠈⠿⣮⡳⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h1>
             <h1>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠶⠤⣤⣀⡙⣿⣿⣿⣿⣯⣿⠷⠶⢦⠈⠙⢶⣝⠶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h1>
             <h1>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠶⢾⣯⡀⠀⠀⠀⠀⢩⣝⣿⣿⣿⣿⣤⣠⠤⠾⢲⠀⠀⠈⢻⣽⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h1>
             <h1>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠹⣆⣆⠟⠆⠀⢀⣠⣴⣏⣿⣿⣿⣿⣿⣥⣀⣴⡲⢾⡧⣤⡀⠘⡏⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h1>
@@ -102,17 +134,17 @@ function Home() {
             <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⢣⠸⣿⣿⣿⡾⣿⣿⣿⣿⣿⣿</h1>
             <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠸⡂⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿</h1>
             <h1>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⡇⢸⣿⣿⣿⣿⣹⣿⣿⣿⣿</h1> */}
-        </div>
+      </div>
 
-        <div className='absolute bottom-0 w-full '>
-            <div className='bottom-10 flex absolute justify-center p-5 w-full'>
-            <button className='bg-[#000] text-[#C8F73A] h-12 w-[90%] rounded-xl'>
-                <Link to='/paypalemail'>
-                    Begin
-                </Link>
-            </button>
-            </div>
+      <div className='absolute bottom-0 w-full '>
+        <div className='bottom-10 flex absolute justify-center p-5 w-full'>
+          <button className='bg-[#000] text-[#C8F73A] h-12 w-[90%] rounded-xl'>
+            <Link to='/paypalemail'>
+              Begin
+            </Link>
+          </button>
         </div>
+      </div>
     </div>
   )
 }
--- src/pages/WalletAddress.js
+++ src/pages/WalletAddress.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
 import { useActiveAccount } from 'thirdweb/react'
 
 function WalletAddress() {
+  const [receiver, setReceiver] = useState("");
+
+  const activeAccount = useActiveAccount();
+
+  const handleReceiverConnected = () => setReceiver(activeAccount?.address);
+
   return (
     <div className='bg-[#C8F73A] relative h-screen'>
 
-        <div className='bg-[#000000] w-full rounded-t-[32px] bottom-0 absolute h-[95%]'>
-            <div className='h-[25%] flex justify-center items-center'>
-                {/* <img src={logo} className='w-28 aspect-square rounded-2xl'/> */}
-                <h1 className='text-[54px] text-[#C8F73A] font-bold'>ZEKE</h1>
+      <div className='bg-[#000000] w-full rounded-t-[32px] bottom-0 absolute h-[95%]'>
+        <div className='h-[25%] flex justify-center items-center'>
+          {/* <img src={logo} className='w-28 aspect-square rounded-2xl'/> */}
+          <h1 className='text-[54px] text-[#C8F73A] font-bold'>ZEKE</h1>
 
-            </div>
-            <div className='p-5 m-5'>
-                <div>
-                    <h1 className='text-white text-sm font-semibold'>Enter Wallet Address</h1>
-                </div>
-                <input placeholder='0x12345677998765' className='bg-transparent placeholder-[#c8f73a46] text-[#C8F73A] text-2xl outline-none mt-5'/>
-            </div>
-            <div className='absolute flex bottom-10 justify-center w-full p-5'>
-                <button className='bg-[#C8F73A] h-12 w-[90%] rounded-xl'>
-                    <Link to='/continue-one'>
-                        Continue
-                    </Link>
-                </button>
-            </div>
         </div>
+        <div className='p-5 m-5'>
+          <div>
+            <h1 className='text-white text-sm font-semibold'>Enter Wallet Address</h1>
+          </div>
+          <input value={receiver} onChange={(e) => { setReceiver(e.currentTarget.value) }} placeholder='0x12345677998765' className='bg-transparent placeholder-[#c8f73a46] text-[#C8F73A] text-2xl outline-none mt-5' />
+          <h1 className='text-blue-500 font-semibold mt-3' onClick={handleReceiverConnected}>Send to connected wallet</h1>
+        </div>
+        <div className='absolute flex bottom-10 justify-center w-full p-5'>
+          <button className='bg-[#C8F73A] h-12 w-[90%] rounded-xl'>
+            <Link to='/continue-one'>
+              Continue
+            </Link>
+          </button>
+        </div>
+      </div>
 
     </div>
   )
