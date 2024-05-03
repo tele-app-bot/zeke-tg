4e0016cba07522831ba62f4379ff84418369f093
ParanoidSyntaxError <71923508+ParanoidSyntaxError@users.noreply.github.com>
2024-05-03 23:19:24 +1000

    Fixed thirdweb

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
-import { ConnectButton } from "thirdweb/react";
-import { sepolia } from "thirdweb/chains";
+import {
+  ConnectButton,
+} from "thirdweb/react";
 import { createThirdwebClient } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import {
+  createWallet,
+  walletConnect,
+  inAppWallet,
+  preAuthenticate
+} from "thirdweb/wallets";
 
-const client = createThirdwebClient({ clientId: "dc7468c96c27ede108735c2ce3ff91cc" });
+const client = createThirdwebClient({
+  clientId: "a02abdc106da3c1467ba518489b891dc",
+});
 
+const wallets = [
+  walletConnect(),
+  inAppWallet({
+    auth: {
+      options: ["email", "phone"],
+    },
+  }),
+  createWallet("io.metamask")
+];
 
 function Home() {
   return (
     <div className='bg-[#000000] relative h-screen'>
-        <div className='h-[25%] flex justify-center items-center'>
-            <img src={logo} className='w-28 aspect-square rounded-2xl'/>
-        </div>
-        <div>
-            <ConnectButton
-                client={client}
-                accountAbstraction={{
-                    chain: sepolia,
-                    factoryAddress: "your-factory-address",
-                    gasless: true,
-                }}
-            />
-        </div>
-        <div className='absolute flex bottom-0 w-full p-5'>
-            <button className='bg-[#C8F73A] h-16 w-full rounded-xl'>
-                <Link to='/walletaddress'>
-                    Begin
-                </Link>
-            </button>
-        </div>
+      <div className='h-[25%] flex justify-center items-center'>
+        <img src={logo} className='w-28 aspect-square rounded-2xl' />
+      </div>
+      <div>
+        <ConnectButton
+          client={client}
+          theme={"dark"}
+          connectModal={{
+            size: "compact",
+            title: "Zeke",
+            titleIcon: "",
+            showThirdwebBranding: false,
+          }}
+          wallets={wallets}
+          accountAbstraction={{
+            chain: baseSepolia,
+            factoryAddress: "0xb26BE4505119107f9Bb6D4948360E4B7Dea50e29",
+            gasless: true,
+          }}
+        />
+      </div>
+      <div className='absolute flex bottom-0 w-full p-5'>
+        <button className='bg-[#C8F73A] h-16 w-full rounded-xl'>
+          <Link to='/walletaddress'>
+            Begin
+          </Link>
+        </button>
+      </div>
     </div>
   )
 }
--- src/pages/WalletAddress.js
+++ src/pages/WalletAddress.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import { useActiveAccount } from 'thirdweb/react'
 
 function WalletAddress() {
+  const activeAccount = useActiveAccount();
+    
   return (
     <div className='bg-[#000000] relative h-screen'>
         <div className='h-[25%] flex justify-center items-center'>
@@ -12,7 +15,7 @@ function WalletAddress() {
             <div>
                 <h1 className='text-white text-xl font-semibold'>Enter Wallet Address</h1>
             </div>
-            <input placeholder='0x12345677998765' className='bg-transparent text-white text-3xl outline-none mt-5'/>
+            <input placeholder={activeAccount?.address} className='bg-transparent text-white text-3xl outline-none mt-5'/>
         </div>
         <div className='absolute flex bottom-0 w-full p-5'>
             <button className='bg-[#C8F73A] h-16 w-full rounded-xl'>
